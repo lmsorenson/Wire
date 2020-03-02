1115fef2b8ab2e48f5db0eafec83143644e0ab07
Lucas Sorenson <lsorenson20@gmail.com>
2020-03-01 21:13:48 -0600

    organize .h file

--- WireLib/include/wire.h
+++ WireLib/include/wire.h
@@ -4,15 +4,13 @@
 
 
 int openSerialPort(const char *bsdPath);
-char *logString(const char * str);
-char *logData(const char * str);
 int initializeModem(int fileDescriptor);
-void writeSerialPort(int fileDescriptor, char * InString);
-int readSerialPort(int fileDescriptor, char * dst);
 int closeSerialPort(int fileDescriptor);
 
-int GetDescriptorValue(char str[], char ch);
+void writeSerialPort(int fileDescriptor, char * InString);
+int readSerialPort(int fileDescriptor, char * dst);
 
+int GetDescriptorValue(char str[], char ch);
 int listDevices(char * search_directory, char * dst);
 
 
